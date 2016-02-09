7ab60e5ac4a831b434c0af67f1b9ecbd8e9a98e6
yuga92 <yurahalych@gmail.com>
2016-02-09 16:56:25 +0200

    API class edited: method GET and AsyncTask class

--- src/com/example/Food2Fork/Api.java
+++ src/com/example/Food2Fork/Api.java
@@ -17,43 +17,52 @@ import java.io.InputStreamReader;
  * receive data from server and transfers to JsonParce
  */
 
-public class Api extends MainActivity {
-    static String uri;
+public class Api{
+    //static String uri;
     static String getUrl = "http://food2fork.com/api/get?";
     static String searchUrl = "http://food2fork.com/api/search?";
-    static int wichActivity;
+    static int whichActivity;
+
+   static  HttpClient httpclient = new DefaultHttpClient();
+    static HttpGet requestURI;
 
     public static String generateGetUri(String key, String recipeId) {
-        return uri = getUrl + "key=" + key + "&rId=" + recipeId;
+        return  getUrl + "key=" + key + "&rId=" + recipeId;
     }
 
     public static String generateSearchUri(String key, String userRequest, String params) {
         return searchUrl + "key=" + key + userRequest + params;
     }
 
-    public static String connect(String key, String userRequest, String params, MainActivity activity) {
-        uri = generateSearchUri(key, userRequest, params);
-        new HttpAsyncTask(activity).execute(uri);
-        wichActivity = 1;
-        return uri;
+    public static void connect(String key, String userRequest, String params, MainActivity activity) {
+        //uri = generateSearchUri(key, userRequest, params);
+        requestURI = new HttpGet(generateSearchUri(key, userRequest, params));
+        new HttpAsyncTask(activity).execute(requestURI);
+        whichActivity = 1;
+       // return uri;
 
     }
 
     public static void connect(String key, String recipeId, RecipeView activity) {
-        uri = generateGetUri(key, recipeId);
-        new HttpAsyncTask(activity).execute(uri);
-        wichActivity = 2;
+       // uri = generateGetUri(key, recipeId);
+        requestURI = new HttpGet(generateGetUri(key, recipeId));
+
+        new HttpAsyncTask(activity).execute(requestURI);
+        whichActivity = 2;
 
     }
 
-    public static String GET(String url) {
+    public static String GET(HttpGet requestURI) {
         InputStream inputStream;
         String result = "";
 
         try {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+           // HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse httpResponse = httpclient.execute(requestURI);
             inputStream = httpResponse.getEntity().getContent();
+
+
+
             if (inputStream != null) {
                 result = convertInputStreamToString(inputStream);
             } else
@@ -75,7 +84,7 @@ public class Api extends MainActivity {
         return result;
     }
 
-    private static class HttpAsyncTask extends AsyncTask<String, Void, String> {
+    private static class HttpAsyncTask extends AsyncTask<HttpGet, Void, String> {
         public MainActivity activity;
         public RecipeView recipeView;
         public HttpAsyncTask(MainActivity a) {
@@ -84,16 +93,18 @@ public class Api extends MainActivity {
         public HttpAsyncTask(RecipeView a) {
             this.recipeView = a;
         }
+
+
         @Override
-        protected String doInBackground(String... urls) {
-            return GET(urls[0]);
+        protected String doInBackground(HttpGet... httpGets) {
+            return GET(requestURI);
         }
 
         @Override
         protected void onPostExecute(String result) {
             //Used callback method in asynctask...
             JsonParce parse = new JsonParce();
-            switch (wichActivity) {
+            switch (whichActivity) {
                 case 1:
                     try {
                         parse.parseJsonSearchRespone(result);
